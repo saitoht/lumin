a8660dca521f2f0124a612fb2db589c73726e64e
saitoht <saito.harutaka@mat.eng.osaka-u.ac.jp>
2024-03-09 13:57:21 +0900

    add lumin.py & cls_subs.py

---
+++ lumin.py
@@ -0,0 +1,26 @@
+### Lumin.py: Estimation of luminescent properties of materials
+###           Consolidate cls_emod.py, cls_ccd.py, and cls_ph.py
+### H. Saito  9th, Mar., 2024
+import sys
+import cls_emod as emod
+import cls_ccd as ccd
+import cls_ph as ph
+import cls_subs as subs
+
+if __name__=="__main__":
+    const = subs.subs()
+    prms = subs.get_prms()
+    print("* --- lumin.py --- *")
+    print("*")
+    if ( prms.sw_run_emod == True ):
+        # Elastic moduli
+        emod.run_emod()
+    if ( prms.sw_run_ph == True ):
+        # Phonon
+        ph.run_phonon()
+    if ( prms.sw_run_ccd == True ):
+        # configuration coordinate model
+        ccd.run_ccd()
+    print("* --- All calculations are finished! --- *")
+    print("*")
+    sys.exit()
